43557ac30d993490fac698ca835ba6dcb71a7f78
Ali RajabNezhad <alirn76@yahoo.com>
2024-01-29 01:58:38 +0330

    `demo` workflow is ok now

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,15 @@
 #! /bin/bash
 
-if [ $1 == "main" ]; then
-  branch=main
-  image=main
+if [ "$1" == "main" ]; then
+  branch="main"
+  image="main"
 else
-  branch=development
-  image=development
+  branch="development"
+  image="development"
 fi
 
 cd carshenas-frontend
-git checkout $branch
-git pull origin $branch
-docker compose build $image
-docker compose up --force-recreate $image -d
+git checkout "$branch"
+git pull origin "$branch" --rebase
+docker compose build "$image"
+docker compose up --force-recreate "$image" -d
