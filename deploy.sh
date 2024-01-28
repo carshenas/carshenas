b80c2efbef2bd1ef71ae459b5d9fcf20de6660ac
Ali RajabNezhad <alirn76@yahoo.com>
2024-01-29 01:10:57 +0330

    Fix `deploy.sh`

--- deploy.sh
+++ deploy.sh
@@ -2,13 +2,15 @@
 
 if [ $1 == "main" ]; then
   branch = main
+  image = main
 else
   branch = development
+  image = development
 fi
 
 cd carshenas-frontend
 git checkout $branch
 git pull origin $branch
-docker compose build
+docker compose build $image
 docker compose down
 docker compose up -d
