9e54d1cfca822c92e66510abdafd27b2c8d3e8b3
Ali RajabNezhad <alirn76@yahoo.com>
2024-01-29 00:50:44 +0330

    Add `demo` workflow

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,16 @@
 #! /bin/bash
 
-cd carshenas-frontend
-git pull origin main
-docker compose build
-docker compose down
-docker compose up -d
+if [ $1 == "main" ]; then
+  branch = main
+  image = main
+else
+  branch = development
+  image = development
+fi
 
+cd carshenas-frontend
+git checkout $branch
+git pull origin $branch
+docker compose build $image
+docker compose down $image
+docker compose up -d $image
