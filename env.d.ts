871f0374e1a3d386fe548c996a3fe5f8e75755a8
sadeq76 <sadeqshahmoradi76@gmail.com>
2024-02-03 00:54:26 +0330

    Add .env file and env.d.ts

--- env.d.ts
+++ env.d.ts
@@ -1,1 +1,17 @@
-/// <reference types="vite/client" />
+/* eslint-disable */
+
+declare namespace NodeJS {
+  interface ProcessEnv {
+    APP_VERSION: string | undefined
+    APP_STAGE: 'DEVELOPMENT' | 'PRODUCTION' | undefined
+
+    // api
+    SSL_ENABLED: 'true' | 'false' | undefined
+    API_SERVER: string | undefined
+
+    // app config
+    BASE_CURRENCY_UNIT: 'IRT' | 'IRR' | undefined
+    DISPLAY_CURRENCY_UNIT: 'IRT' | 'IRR' | undefined
+    WEBSITE_URL: string | undefined
+  }
+}
