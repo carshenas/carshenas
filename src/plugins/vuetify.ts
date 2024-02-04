91e2fd3e8813abf5b9287257c9ce7f99995447ac
sadeq76 <sadeqshahmoradi76@gmail.com>
2024-02-04 22:25:15 +0330

    Add i18n and change icons from mdi to md

--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -1,12 +1,31 @@
+import { md3 } from 'vuetify/blueprints'
+import { createVuetify } from 'vuetify'
+
+// Icons
+import 'material-design-icons-iconfont/dist/material-design-icons.css'
+import { aliases, md } from 'vuetify/iconsets/md'
+
 // Styles
-import '@mdi/font/css/materialdesignicons.css'
 import 'vuetify/styles'
 
-import { createVuetify } from 'vuetify'
-import { md3 } from 'vuetify/blueprints'
+// I18n
+import { createVueI18nAdapter } from 'vuetify/locale/adapters/vue-i18n'
+import { useI18n } from 'vue-i18n'
+
+import i18n from './i18n'
 
 export default createVuetify({
   blueprint: md3,
+  locale: {
+    adapter: createVueI18nAdapter({ i18n, useI18n })
+  },
+  icons: {
+    defaultSet: 'md',
+    aliases,
+    sets: {
+      md
+    }
+  },
   theme: {
     themes: {
       light: {
