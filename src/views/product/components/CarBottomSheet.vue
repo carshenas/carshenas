308c8e48730e86d6e7a4e760a35953159eece112
BenGix <84526926+BenGix@users.noreply.github.com>
2024-02-18 00:35:35 +0330

    fab button animation

--- src/views/product/components/CarBottomSheet.vue
+++ src/views/product/components/CarBottomSheet.vue
@@ -1,24 +1,19 @@
-
-
-
 <script setup lang="ts">
 import { ref } from 'vue'
 import type Brands from '@/types/dto/brands'
 const sheet = ref(false)
-const showText = ref(true)
 
-setTimeout(() => {
-  showText.value = false
-}, 1000)
 
 const carBrands = ref<Brands[]>([
   { id: 1, icon: "../../../../public/icons/battery.svg", name: "سایپا", models: ["کوییک", "ساینا", "پراید"] },
   { id: 1, icon: "../../../../public/icons/battery.svg", name: "ایرانخودرو", models: ["تارا", "سمند", "دنا"] },
 ])
+
 const selectedBrand = ref<Brands | null>(null)
 const selectBrand = (brand: Brands) => {
   selectedBrand.value = brand
 }
+
 const goBack = () => {
   selectedBrand.value = null
 }
@@ -27,23 +22,22 @@ const goBack = () => {
 
 <template>
   <v-bottom-sheet v-model="sheet">
-    <template v-slot:activator="{ props }">
 
-      <div class="text-center position-fixed carFitlerPlaceHolder">
-        <v-btn v-if="showText" v-bind="props" color="primary" rounded="pill" size="x-large" class="position-fixed"
-          :key="showText">
+    <template v-slot:activator="{ props }">
+      <div class="fabBtnContainer">
+        <v-btn v-bind="props" color="white" rounded="pill" size="x-large" class="fabBtn-text pr-8" variant="text"
+          key="showText">
           {{ $t('shared.carFilter') }}
           <template v-slot:prepend>
             <v-icon icon="directions_car" />
           </template>
         </v-btn>
-
-        <v-btn cl v-else v-bind="props" icon="directions_car" color="primary" rounded="pill" class="position-fixed" />
+        <v-btn v-bind="props" size="large" class="fabBtn-icon" icon="directions_car" color="primary" rounded="pill" />
       </div>
     </template>
 
     <v-list v-if="!selectedBrand">
-      <v-list-item v-for="Brands in carBrands " :key="Brands.id" :prepend-avatar="Brands.icon"
+      <v-list-item v-for="Brands in carBrands" :key="Brands.id" :prepend-avatar="Brands.icon"
         @click="selectBrand(Brands)">
 
         <span>
@@ -51,7 +45,7 @@ const goBack = () => {
         </span>
       </v-list-item>
     </v-list>
-    
+
     <v-list v-if="selectedBrand">
       <v-btn variant="text" color="text" @click="goBack">
         <template v-slot:prepend>
@@ -66,5 +60,40 @@ const goBack = () => {
   </v-bottom-sheet>
 </template>
 
+<style lang="scss" scoped>
+@keyframes changeWidth {
+  0% {
+    width: 10rem;
+  }
 
+  100% {
+    width: 3.5rem;
+  }
+}
 
+.fabBtnContainer {
+  position: fixed;
+  bottom: 1rem;
+  left: 1rem;
+  overflow: hidden;
+  height: 3.5rem;
+  width: 10rem;
+  z-index: 1000;
+  animation-name: changeWidth;
+  animation-duration: .5s;
+  animation-delay: 5s;
+  animation-fill-mode: forwards;
+  background-color: #fb4847;
+  border-radius: 999px;
+}
+
+.fabBtn-text {
+  position: absolute;
+  left: 0;
+}
+
+.fabBtn-icon {
+  position: absolute;
+  right: 0;
+}
+</style>
