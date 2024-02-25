38019caf99376a98be311696f3ce46cc2eda1cd5
sadeq76 <sadeqshahmoradi76@gmail.com>
2024-02-25 20:29:55 +0330

    Update .env and change server api and update fetcher

--- src/lib/fetcher/constants.ts
+++ src/lib/fetcher/constants.ts
@@ -1,7 +1,11 @@
 import type { Scheme } from './types'
 
 export const defaultHeaders = {
-  'content-type': 'application/json'
+  Accept: 'application/json',
+  'Content-Type': 'application/json',
+  'Accept-Language': 'en-US',
+  'Cache-Control': 'no-cache',
+  Connection: 'keep-alive'
 }
 
 export const defaultScheme: Scheme = 'https'
