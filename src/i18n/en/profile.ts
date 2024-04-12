e8bdfa4817bc2f31d1442bcc110e49c741149331
Ali RajabNezhad <alirn76@yahoo.com>
2024-04-12 18:40:03 +0330

    Profile (#21)
    * profile setup
    
    setting up user profile
    
    * address page
    
    * order page setup
    
    * orders final
    
    * remove profile for lower case naming
    
    * adding profile with corroct name
    
    * Support main page
    
    support main page final
    
    * support chat history
    
    * support ui final
    
    * hot fix
    
    * notif page
    
    * routingFixed
    
    * v-list added to profile
    
    * remove vscodesetting
    
    * delete vs code
    
    ---------
    
    Co-authored-by: BenGix <84526926+BenGix@users.noreply.github.com>

---
+++ src/i18n/en/profile.ts
@@ -0,0 +1,14 @@
+export default {
+  userInfo: 'User info',
+  name: 'Fame',
+  lastName: 'Last name',
+  phoneNumber: 'phone number',
+  email: 'Email',
+  nationalCode: 'کد ملی',
+  addresses: 'Addresses',
+  orders: 'Orders',
+  support: 'Support',
+  notification: 'Notification',
+  logout: 'Logout',
+  newTicket: 'New ticket',
+}
