a92d2aa90f0f7a77f670761cdb396c0b9f303ebb
Sadeq Shahmoradi <sadeqshahmoradi76@gmail.com>
2024-08-02 21:50:50 +0330

    Feature/complete landing search auth (#42)
    * Fix buttons size
    
    * Add empty state
    
    * Make top bar sticky
    
    * Add skeleton and fabtbn
    
    * Add auth
    
    * Unfinished commit
    
    * Unfinished commit
    
    * Update services
    
    * Update pnpm lock

--- src/services/carshenas/category/index.ts
+++ src/services/carshenas/category/index.ts
@@ -1,6 +1,9 @@
 import carshenasService from '@/services'
 import type { Category, CategoryFilter } from '@/types/dto/category'
 
+// export const getCategoryListService = (parameters?: CategoryFilter) =>
+//   carshenasService.get<Category[], never>('/category', { parameters })
+
 export const getCategoryListService = (parameters?: CategoryFilter) =>
   carshenasService.get<Category[]>('/category', { parameters })
 
