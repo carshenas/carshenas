3bc34c92a3be9e62d8bdbdbb4374da026eb9a75d
Sadeq Shahmoradi <sadeqshahmoradi76@gmail.com>
2024-08-17 23:31:56 +0330

    Development (#48)
    * update gitignore
    
    * first commit

--- src/components/ImageLoader.vue
+++ src/components/ImageLoader.vue
@@ -1,59 +1,75 @@
 <script setup lang="ts">
-import useAppConfig from '@/composable/app-config'
-import { computed, onMounted, ref, watch } from 'vue'
-import IMAGES_TYPES from '@/constants/supported-image-types'
-import type { ImageExtension } from '@/types/image.ts'
-import type { NullableString } from '@/types/global'
-import { getAssetUrl } from '@/helpers/files'
+import useAppConfig from "@/composable/app-config";
+import { computed, onMounted, ref, watch } from "vue";
+import IMAGES_TYPES from "@/constants/supported-image-types";
+import type { ImageExtension } from "@/types/image.ts";
+import type { NullableString } from "@/types/global";
+import { getAssetUrl } from "@/helpers/files";
+import { WIDTH_PATTERN, HEIGHT_PATTERN } from "@/constants/regexp";
 
-const appConfig = useAppConfig()
+const appConfig = useAppConfig();
 const props = defineProps<{
-  src: NullableString
-  width?: string
-  height?: string
-  alt: string
-  types?: ImageExtension[]
-  aspectRatio?: string
-}>()
-const picture = ref<HTMLPictureElement>()
-const images = ref<any[]>([])
-const defaultImage = ref<string | undefined>()
+  src: NullableString;
+  width?: string;
+  height?: string;
+  alt: string;
+  types?: ImageExtension[];
+  aspectRatio?: string;
+}>();
+const picture = ref<HTMLPictureElement>();
+const images = ref<any[]>([]);
+const defaultImage = ref<string | undefined>();
 
 const filteredImageTypes = computed(() =>
-  props.types?.length ? IMAGES_TYPES.filter((item) => props.types!.includes(item.extension)) : []
-)
+  props.types?.length
+    ? IMAGES_TYPES.filter((item) => props.types!.includes(item.extension))
+    : []
+);
 
 const generateSrc = async (path: string): Promise<string> => {
-  if (path.startsWith('@')) return await getAssetUrl(path)
-  if (path.startsWith('http') && !path.startsWith(appConfig.staticFileServer))
-    return (defaultImage.value = props.src!)
+  if (path.startsWith("@")) return await getAssetUrl(path);
+  if (path.startsWith("http") && !path.startsWith(appConfig.staticFileServer))
+    return (defaultImage.value = props.src!);
 
-  const width = props.width?.includes('%') ? picture.value?.offsetWidth : props.width
-  const height = props.width?.includes('%') ? picture.value?.offsetWidth : props.width
+  const width = props.width?.includes("%")
+    ? picture.value?.offsetWidth
+    : props.width;
+  const height = props.width?.includes("%")
+    ? picture.value?.offsetWidth
+    : props.width;
 
-  return path.replace('%7BWIDTH%7D', String(width)).replace('%7BHEIGHT%7D', String(height))
-}
+  return path
+    .replace(WIDTH_PATTERN, String(width))
+    .replace(HEIGHT_PATTERN, String(height));
+};
 
 const generateImagesUrl = async () => {
-  if (!props.src) return
-  images.value = []
+  if (!props.src) return;
+  images.value = [];
   filteredImageTypes.value.forEach(async (type) =>
-    props.src ? images.value?.push({ src: await generateSrc(props.src), ...type }) : undefined
-  )
-  defaultImage.value = props.src ? await generateSrc(props.src) : undefined
-}
+    props.src
+      ? images.value?.push({ src: await generateSrc(props.src), ...type })
+      : undefined
+  );
+  defaultImage.value = props.src ? await generateSrc(props.src) : undefined;
+};
 
-onMounted(() => generateImagesUrl())
+onMounted(() => generateImagesUrl());
 
 watch(
   () => props.src,
   () => generateImagesUrl()
-)
+);
 </script>
 
 <template>
   <picture v-if="props.src" ref="picture">
-    <source v-for="image in images" :key="image.type" :srcset="image.src" :type="image.type" />
+    <source
+      v-for="image in images"
+      :key="image.type"
+      :srcset="image.src"
+      :type="image.type"
+    />
 
     <img
       :src="defaultImage"
--- src/constants/regexp.ts
+++ src/constants/regexp.ts
@@ -1,8 +1,11 @@
-export const MOBILE_NUMBER_REGEXP = /^(0|\+98|0098)9[0-9]{9}$/
-export const PHONE_NUMBER_REGEXP = /^((0|\+98|0098)[1-9]{2})?[1-9][0-9]{3,7}$/g
-export const NATIONAL_ID_REGEXP = /^(?!(\d)\1{9})[0-9]{10}$/
+export const MOBILE_NUMBER_REGEXP = /^(0|\+98|0098)9[0-9]{9}$/;
+export const PHONE_NUMBER_REGEXP = /^((0|\+98|0098)[1-9]{2})?[1-9][0-9]{3,7}$/g;
+export const NATIONAL_ID_REGEXP = /^(?!(\d)\1{9})[0-9]{10}$/;
 export const EMAIL_REGEXP =
-  /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/g
-export const PERSIAN_NUMBERS_REGEXP = /[۰-۹]/g
-export const ARABIC_NUMBERS_REGEXP = /[٠-٩]/g
-export const IMAGE_FILE_EXTENSIONS_REGEXP = /\.(jpg|jpeg|png|webp|avif|gif|svg)$/
+  /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/g;
+export const PERSIAN_NUMBERS_REGEXP = /[۰-۹]/g;
+export const ARABIC_NUMBERS_REGEXP = /[٠-٩]/g;
+export const IMAGE_FILE_EXTENSIONS_REGEXP =
+  /\.(jpg|jpeg|png|webp|avif|gif|svg)$/;
+export const WIDTH_PATTERN = /%7BWIDTH%7D|{WIDTH}/g;
+export const HEIGHT_PATTERN = /%7BHEIGHT%7D|{HEIGHT}/g;
