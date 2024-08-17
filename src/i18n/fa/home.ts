3e941d5f6e63817411b5b585ab26f84084141a38
BenGix <84526926+BenGix@users.noreply.github.com>
2024-08-17 13:34:17 +0330

    fix desc

--- src/i18n/fa/home.ts
+++ src/i18n/fa/home.ts
@@ -1,8 +1,7 @@
 export default {
-  headline: 'کارشناس فروشگاه قطعات خودرو',
-  description: 'بزرگترین فروشگاه اینترنتی قطعات خودرو',
-  popularModels: 'محبوب ترین مدل ها',
-  popularCatagories: 'محبوب ترین دسته بندی ها',
-  carMagazine: 'مجله خودرو',
-  description: 'بزرگترین فروشگاه اینترنتی قطعات خودرو'
-}
+  headline: "کارشناس فروشگاه قطعات خودرو",
+  description: "بزرگترین فروشگاه اینترنتی قطعات خودرو",
+  popularModels: "محبوب ترین مدل ها",
+  popularCatagories: "محبوب ترین دسته بندی ها",
+  carMagazine: "مجله خودرو",
+};
