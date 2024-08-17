9ed6dc6ddb8628f7007ceb41ddee48f1c94c915a
BenGix <84526926+BenGix@users.noreply.github.com>
2024-08-17 13:30:34 +0330

    fix conflict

--- src/i18n/en/index.ts
+++ src/i18n/en/index.ts
@@ -1,14 +1,13 @@
-import app from './app'
-import currency from './currency'
-import search from './search'
-import shared from './shared'
-import home from './home'
-import user from './user'
-import auth from './auth'
-import validation from './validation'
-import checkout from './checkout'
-import product from './product'
-=======
+import app from "./app";
+import currency from "./currency";
+import search from "./search";
+import shared from "./shared";
+import home from "./home";
+import user from "./user";
+import auth from "./auth";
+import validation from "./validation";
+import checkout from "./checkout";
+import product from "./product";
 
 export default {
   app,
@@ -20,6 +19,5 @@ export default {
   auth,
   user,
   validation,
-  checkout
-
-}
+  checkout,
+};
