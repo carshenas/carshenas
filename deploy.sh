c9a88c9e921ae689bdc0d3b1bd38869f50661535
Ali RajabNezhad <alirn76@yahoo.com>
2024-08-18 00:59:29 +0330

    Use Separated Docker Command in deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,17 +3,20 @@
 source /root/.bashrc
 
 if [ "$1" == "main" ]; then
-  username="$FARZAD_DOCKER_USERNAME"
-  password="$FARZAD_DOCKER_PASSWORD"
+  docker login -u farzadjambor -p "$FARZAD_DOCKER_PASSWORD"
+  docker pull farzadjambor/carshenas:latest
   image="main"
+
 else
-  username="$FARHAD_DOCKER_USERNAME"
-  password="$FARHAD_DOCKER_PASSWORD"
+  docker login -u sadeq76 -p "$FARHAD_DOCKER_PASSWORD"
+  docker pull farzadjambor/carshenas:latest
   image="development"
+
 fi
 
+docker logout
+
+echo "Recreating ..."
 cd carshenas-frontend
-#docker login -u "$username" -p "$password"
-docker pull "$username"/carshenas:latest
-#docker logout
 docker compose up --force-recreate "$image" -d
+echo "Recreated."
