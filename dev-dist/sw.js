93e0f8d42ab7b01ae380be5962ab90804e575dc5
BenGix <84526926+BenGix@users.noreply.github.com>
2024-08-21 00:35:38 +0330

    fix drawer

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-5199072c'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.vq2nn7v42n8"
+    "revision": "0.p9cl3qqnsm8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
