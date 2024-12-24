e6a76ed96277f747c5ca2ea8c3162e96dae87f61
Sadeq Shahmoradi <sadeqshahmoradi76@gmail.com>
2024-12-24 20:26:00 +0330

    Revert "Fix search problem (#72)" (#73)
    This reverts commit 413a25e395ba23395a45d81de28e69e5c03a3b7f.

--- src/stores/category.ts
+++ src/stores/category.ts
@@ -1,41 +1,43 @@
-import { defineStore } from "pinia";
-import { ref } from "vue";
+import { defineStore } from 'pinia'
+import { ref } from 'vue'
 
 // Services
-import { getCategoryListService } from "@/services/carshenas/category";
+import { getCategoryListService } from '@/services/carshenas/category'
 
 // type
-import type { Category } from "@/types/dto/category";
+import type { Category } from '@/types/dto/category'
 
-export const useCategoryStore = defineStore("category", () => {
-  const categories = ref<Category[]>([]);
+export const useCategoryStore = defineStore('category', () => {
+  const categories = ref<Category[]>([])
 
   const getCategories = async () => {
-    if (categories.value && categories.value?.length) return;
+    if (categories.value && categories.value?.length) return
 
     try {
-      const { data } = await getCategoryListService();
+      const { data } = await getCategoryListService()
 
-      categories.value = data;
+      categories.value = data
     } catch (e) {
-      console.error(e);
+      console.error(e)
     }
-  };
+  }
 
-  const filterByTitle = (
-    categories: Category[],
-    searchTerm: string
-  ): Category[] => {
-    const results: Category[] = [];
+  const filteredCategories = (categories: Category[], searchTerm: string): Category[] => {
+    const results: Category[] = []
 
     categories.forEach((category) => {
       if (category.name.toLowerCase().includes(searchTerm.toLowerCase())) {
-        results.push(category);
+        results.push(category)
       }
-    });
 
-    return results;
-  };
+      if (category.children) {
+        const childResults = filteredCategories(category.children, searchTerm)
+        results.push(...childResults)
+      }
+    })
 
-  return { categories, getCategories, filterByTitle };
-});
+    return results
+  }
+
+  return { categories, getCategories, filteredCategories }
+})
