9cd950b501389e268a151fd2d8349e1e6338e589
Farzad Jambor Shabestari <84526926+BenGix@users.noreply.github.com>
2024-12-27 22:57:58 +0330

    fix bugs

--- src/composable/app-config.ts
+++ src/composable/app-config.ts
@@ -7,8 +7,8 @@ const schema =
 export default function useAppConfig() {
   return {
     appVersion: import.meta.env.VITE_APP_VERSION || null,
-    baseCurrencyUnit: import.meta.env.VITE_BASE_CURRENCY_UNIT || 'IRR',
-    displayCurrencyUnit: import.meta.env.VITE_DISPLAY_CURRENCY_UNIT || 'IRR',
+    baseCurrencyUnit: import.meta.env.VITE_BASE_CURRENCY_UNIT || 'IRT',
+    displayCurrencyUnit: import.meta.env.VITE_DISPLAY_CURRENCY_UNIT || 'IRT',
     apiServer: `${schema}${import.meta.env.VITE_API_SERVER}`,
     staticFileServer: import.meta.env.VITE_API_STATIC_SERVER
   } as AppConfig
