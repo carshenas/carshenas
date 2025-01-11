28d83f37fdf1c1c36158fcf8367167f48dbd07ee
sadeq76 <sadeqshahmoradi76@gmail.com>
2025-01-11 17:55:25 +0330

    Unfinished commit

--- src/components/ProductList.vue
+++ src/components/ProductList.vue
@@ -1,6 +1,6 @@
 <script lang="ts" setup>
 import type { Product, ProductFilter, Variant } from '@/types/dto/product'
-import { ref, onMounted, watch, computed } from 'vue'
+import { ref, onMounted, watch, computed, reactive } from 'vue'
 import { getProductListService } from '@/services/carshenas/product'
 import CurrencyDisplay from './CurrencyDisplay.vue'
 import ImageLoader from './ImageLoader.vue'
@@ -17,37 +17,53 @@ const props = defineProps<{
   manual?: boolean
 }>()
 
-const _loading = ref<boolean>(false)
+const isLoading = ref<boolean>(false)
 const products = ref<Product[]>(props.items || [])
+const pagination = reactive<{
+  limit: number
+  offset: number
+}>({ limit: 10, offset: 0 })
 
 const shouldFetchProducts = computed(
   () => !props.items || props.items.length === 0
 )
 
-const getProducts = async () => {
+const getProducts = async ({
+  done
+}: {
+  done: (status: 'ok' | 'error' | 'empty' | 'loading') => void
+}) => {
   if (!shouldFetchProducts.value) return
 
-  _loading.value = true
+  isLoading.value = true
   try {
-    const response = await getProductListService(props.filter)
-    products.value = response.data.result
+    const response = await getProductListService({
+      ...props.filter,
+      ...pagination
+    })
+    products.value = products.value.concat(response.data.result)
+
+    pagination.offset += pagination.limit
+
+    done('ok')
   } catch (e) {
     console.error(e)
+    done('error')
   } finally {
-    _loading.value = false
+    isLoading.value = false
   }
 }
 
-watch(
-  () => props.filter,
-  () => getProducts()
-)
+// watch(
+//   () => props.filter,
+//   () => getProducts()
+// )
 
-onMounted(() => {
-  if (!props.manual) {
-    getProducts()
-  }
-})
+// onMounted(() => {
+//   if (!props.manual) {
+//     getProducts()
+//   }
+// })
 
 const handleItemCounter = (product: Product, quantity: number) => {
   const existingItem = cartStore.items.find((item) => item.id === product.id)
@@ -84,83 +100,86 @@ const getCartVariant = (productId: number): Variant | null => {
 </script>
 
 <template>
-  <div v-if="_loading || props.loading">
-    <v-skeleton-loader
-      v-for="n in 4"
-      :key="n"
-      class="mt-2"
-      height="100"
-      type="ossein"
-    />
-  </div>
-
-  <v-list v-else>
-    <v-list-item
-      v-for="product in products"
-      :key="product.id"
-      class="product py-2 px-4"
-    >
-      <v-row>
-        <v-col cols="4" class="d-flex align-center">
-          <ImageLoader
-            :src="
-              product.images && product.images.length > 0
-                ? product.images[0]
-                : product.image || 'placeholder.jpg'
-            "
-            :alt="product.name!"
-            width="86"
-            height="86"
-            aspectRatio="1"
-          />
-        </v-col>
-
-        <v-col cols="8" class="d-flex flex-column">
-          <div class="d-flex justify-space-between align-center">
-            <h2 class="title-sm">{{ product.name }}</h2>
-
-            <v-btn
-              v-if="props.hasCounter && getCartQuantity(product.id) > 0"
-              density="compact"
-              icon="delete"
-              variant="plain"
-              class="px-0"
-            />
-          </div>
-
-          <p class="body-sm mt-2 text-outline">{{ product.description }}</p>
-
-          <div class="mt-2 flex-grow-1 d-flex justify-space-between align-end">
-            <CurrencyDisplay
-              :value="product.price"
-              value-class="text-primary font-weight-bold"
-              unit-class="body-sm text-outline"
-              class="d-flex justify-end body-md py-1"
-            />
-
-            <ItemCounter
-              v-if="props.hasCounter && getCartVariant(product.id)"
-              :variant="getCartVariant(product.id)!"
-              :quantity="getCartQuantity(product.id)"
-              @update:quantity="
-                (quantity) => handleItemCounter(product, quantity)
+  <v-infinite-scroll @load="getProducts">
+    <template v-for="product in products" :key="product.id">
+      <div class="product py-2 px-4">
+        <v-row>
+          <v-col cols="4" class="d-flex align-center">
+            <ImageLoader
+              :src="
+                product.images && product.images.length > 0
+                  ? product.images[0]
+                  : product.image || 'placeholder.jpg'
               "
+              :alt="product.name!"
+              width="86"
+              height="86"
+              aspectRatio="1"
             />
+          </v-col>
 
-            <v-btn
-              v-else
-              :text="$t('shared.more')"
-              variant="plain"
-              class="px-0"
-              append-icon="chevron_left"
-              density="compact"
-              :to="{ name: 'ProductDetailPage', params: { id: product.id } }"
-            ></v-btn>
-          </div>
-        </v-col>
-      </v-row>
-    </v-list-item>
-  </v-list>
+          <v-col cols="8" class="d-flex flex-column">
+            <div class="d-flex justify-space-between align-center">
+              <h2 class="title-sm">{{ product.name }}</h2>
+
+              <v-btn
+                v-if="props.hasCounter && getCartQuantity(product.id) > 0"
+                density="compact"
+                icon="delete"
+                variant="plain"
+                class="px-0"
+              />
+            </div>
+
+            <p class="body-sm mt-2 text-outline">{{ product.description }}</p>
+
+            <div
+              class="mt-2 flex-grow-1 d-flex justify-space-between align-end"
+            >
+              <CurrencyDisplay
+                :value="product.price"
+                value-class="text-primary font-weight-bold"
+                unit-class="body-sm text-outline"
+                class="d-flex justify-end body-md py-1"
+              />
+
+              <ItemCounter
+                v-if="props.hasCounter && getCartVariant(product.id)"
+                :variant="getCartVariant(product.id)!"
+                :quantity="getCartQuantity(product.id)"
+                @update:quantity="
+                  (quantity: number) => handleItemCounter(product, quantity)
+                "
+              />
+
+              <v-btn
+                v-else
+                :text="$t('shared.more')"
+                variant="plain"
+                class="px-0"
+                append-icon="chevron_left"
+                density="compact"
+                :to="{
+                  name: 'ProductDetailPage',
+                  params: { id: product.id }
+                }"
+              />
+            </div>
+          </v-col>
+        </v-row>
+      </div>
+    </template>
+
+    <template #loading>
+      <v-skeleton-loader
+        v-for="n in 4"
+        :key="n"
+        class="mt-2"
+        height="100"
+        type="ossein"
+      />
+    </template>
+  </v-infinite-scroll>
 </template>
 
 <style scoped>
