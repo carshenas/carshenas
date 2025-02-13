9bbb8c626bd7f261a07a5ec1bc8bcf10a5af37bc
Farzad Jambor Shabestari <84526926+BenGix@users.noreply.github.com>
2025-02-13 13:21:37 +0330

    Merge pull request #106 from carshenas/development
    Development

--- src/views/user/orders/components/OrderCards.vue
+++ src/views/user/orders/components/OrderCards.vue
@@ -1,32 +1,32 @@
 <script setup lang="ts">
-import type {OrderListResponse} from "@/types/dto/order";
+import type { OrderListResponse } from "@/types/dto/order";
 import { toDisplayCurrency } from "@/helpers/currency";
 import { useJalaliDate } from '@/composable/use-jalali-date'
 
 const props = defineProps<{
   order: OrderListResponse;
-  toggleOrderDetail: (index: number) => void;
-  index: number;
+  toggleOrderDetail: (id: number) => void;
+  id: number;
 }>();
 const getStateData = (status: string) => {
   switch (status) {
     case 'Created':
       return {
-        class: 'text-green-darken-4',
-        text: 'تحویل داده شد',
-        icon: 'done'
+        class: 'text-yellow-darken-2',
+        text: 'در انتظار پرداخت',
+        icon: 'schedule'
       };
     case 'InProgress':
       return {
-        class: 'text-orange',
+        class: 'text-orange-darken-4',
         text: 'درجریان',
         icon: 'pending'
       };
-    case 'canceled':
+    case 'Sent':
       return {
-        class: 'text-red-darken-4',
+        class: 'text-green-darken-2',
         text: 'لغو شده',
-        icon: 'close'
+        icon: 'done'
       };
     default:
       return {
@@ -46,19 +46,19 @@ const { convertToJalali } = useJalaliDate()
       <span :class="getStateData(order.status).class">{{ getStateData(order.status).text }}
         <v-icon :icon="getStateData(order.status).icon" size="x-small" />
       </span>
-      <v-btn icon="more_horiz" variant="text" @click="() => toggleOrderDetail(index)" />
+      <v-btn icon="more_horiz" variant="text" @click="() => toggleOrderDetail(order.id)" />
     </div>
     <div class="d-flex w-100 justify-space-between text-grey">
       <v-card-text class="title-md">{{ order.id }}</v-card-text>
 
       <div class='d-flex justify-space-between w-50 align-center'>
-        <span>{{toDisplayCurrency(order.finalPrice)}}</span>
+        <span>{{ toDisplayCurrency(order.finalPrice) }}</span>
         <div>
+        </div>
+        <div>
+          <span>{{ convertToJalali(order.dateCreated) }}</span>
+        </div>
       </div>
-      <div>
-        <span>{{ convertToJalali(order.dateCreated) }}</span>
-      </div>
-    </div>
 
     </div>
   </v-card>
